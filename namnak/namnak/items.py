042998ed6e8731f334886383a115fbb101642324
hsarena <hmd.salehian@gmail.com>
2019-03-24 12:52:15 +0430

    initial configuration

---
+++ namnak/namnak/items.py
@@ -0,0 +1,8 @@
+from scrapy.item import Item, Field
+
+class NamnakItem(Item):
+    category = Field()
+    group = Field()
+    title = Field()
+    link = Field()
+    summary = Field()
---
+++ namnak/namnak/pipelines.py
@@ -0,0 +1,33 @@
+from sqlalchemy.orm import sessionmaker
+from namnak.models import NamnakDB, db_connect, create_table
+
+
+class NamnakPipeline(object):
+
+    def __init__(self):
+        """
+        Initializes database connection and sessionmaker.
+        Creates deals table.
+        """
+        engine = db_connect()
+        create_table(engine)
+        self.Session = sessionmaker(bind=engine)
+
+    def process_item(self, item, spider):
+        session = self.Session()
+        namnakdb = NamnakDB()
+        namnakdb.category = item['category']
+        namnakdb.group = item['group']
+        namnakdb.title = item['title']
+        namnakdb.summary = item['summary']
+        namnakdb.link = item['link']
+
+        try:
+            session.add(namnakdb)
+            session.commit()
+        except:
+            session.rollback()
+            raise
+        finally:
+            session.close()
+        return item
